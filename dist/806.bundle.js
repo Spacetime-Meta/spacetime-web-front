b85873b36028226db5ff0ba895c28c403f040624
LiquidBlocks <fredcote-_-@hotmail.com>
2022-03-15 21:40:19 -0400

    Delete 806.bundle.js

--- dist/806.bundle.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunknami_wallet_integration=self.webpackChunknami_wallet_integration||[]).push([[806],{7806:e=>{function n(e){var n=new Error("Cannot find module '"+e+"'");throw n.code="MODULE_NOT_FOUND",n}n.keys=()=>[],n.resolve=n,n.id=7806,e.exports=n}}]);
