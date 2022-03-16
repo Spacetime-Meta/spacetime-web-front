fc54ec2a586105a846eef04ceeb742600bfc30db
Frédéric Côté <fredcote-_-@hotmail.com>
2022-03-16 13:54:28 -0400

    Created first proposal

--- src/pages/GovernancePage/GovernancePageComponents/GovernanceProposalsPanel.js
+++ src/pages/GovernancePage/GovernancePageComponents/GovernanceProposalsPanel.js
@@ -1,17 +1,15 @@
 import React from 'react'
-
 import styled from "styled-components";
 
 import { PanelTitle } from "../../../components/sharedComponents/TitleComponents"
 import { Panel } from "../../../components/sharedComponents/Panels"
+import ProposalContainer from "./ProposalContainer.js"
 
 export const GovernanceProposalsPanel = () => {
     return (
         <Panel>
             <PanelTitle>Proposals</PanelTitle>
-            <div style={{height: "100px", width: "100%", textAlign: "center", marginTop: "100px"}}>
-                There is no active proposal
-            </div>
+            <ProposalContainer />
         </Panel>
     )
 }
